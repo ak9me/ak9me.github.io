0112dbe34685fd24544c31a9a726792a22ad39ba
imdiormade <124699001+imdiormade@users.noreply.github.com>
2023-02-24 11:07:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,132 @@
-/* -----------------------------------------------
-/* Author : Titanium Network
-/* MIT license: http://opensource.org/licenses/MIT
-/* ----------------------------------------------- */
-
-const express = require('express'),
-    alloy = require('alloyproxy'),
-    app = express(),
-    http = require('http'),
+const Alloy = require('alloyproxy'),
+    rewrite = require('alloyproxy/libs/rewriting.js'),
+    config = require('./config.json'),
     fs = require('fs'),
-    path = require('path');
+    http = require('http'),
+    https = require('https'),
+    querystring = require('querystring'),
+    express = require('express'),
+    app = express();
+    
+if (!config.prefix.startsWith('/')) config.prefix = '/' + config.prefix;
 
-const config = JSON.parse(fs.readFileSync('./config.json', {
-    encoding: 'utf8'
-}));
+if (!config.prefix.endsWith('/')) config.prefix = config.prefix + '/';
 
-const server = http.createServer(app);
+var server, protocol = 'http://';
 
-//Local Alloy Proxy
+const ssl_options = {
+    key: fs.readFileSync('./ssl/default.key'),
+    cert: fs.readFileSync('./ssl/default.crt')
+};
 
-const localprox = new alloy({
-    prefix: '/prefix/',
-    error: (proxy) => {
-        return proxy.res.send(fs.readFileSync(path.join(__dirname, 'public', 'error.html'), 'utf8'));
-    },
+if (config.ssl == true) { server = https.createServer(ssl_options, app); protocol = 'https://' } else server = http.createServer(app);
+
+
+btoa = (str) => {
+
+	str = new Buffer.from(str).toString('base64');
+	return str;
+
+};
+
+
+atob = (str) => {
+
+    str = new Buffer.from(str, 'base64').toString('utf-8');
+	return str;
+
+};
+
+const Unblocker = new Alloy({
+    prefix: config.prefix,
+    error: (proxy) => { proxy.res.send(fs.readFileSync('./error.html', { encoding: 'utf8' }).replace('%ERR%', proxy.error.info.message.replace(/</gi, '<&zwnj;').replace(/>/gi, '>&zwnj;'))); }, // Doing replace functions on "<" and ">" to prevent XSS.
     request: [],
     response: [],
-    injection: true
-});
+    injection: true,
+});    
+ 
+// The main part of the proxy. 
 
-app.use(localprox.app);
+app.get(config.prefix, (req, res, next) => {
 
-localprox.ws(server);
+    if (req.query.url) {
 
-//Cloudflare Attack Mode Fix
+        var url = atob(req.query.url);
 
-app.post('/', async (req, res) => {
-    switch (req.url) {
-        case '/':
-            return res.send(fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8'));
-    }
-});
+        if (url.startsWith('//')) url = 'http:' + url;
 
-//Querystring Navigation
-app.get('/', async (req, res) => {
+        if (url.startsWith('https://') || url.startsWith('http://')) { return res.redirect(config.prefix + rewrite.url(url)) }
 
-    switch (req.url) {
-        case '/':
-            return res.send(fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8'));
-    }
+        else return res.redirect(config.prefix + rewrite.url('http://' + url))
 
-    switch (req.url) {
-        case '/?a':
-            return res.send(fs.readFileSync(path.join(__dirname, 'public', 'error.html'), 'utf8'));
-    }
+    } else return next();
 
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(config.prefix, (req, res, next) => {
 
-server.listen(process.env.PORT || config.port);
+    req.url = config.prefix + req.url.slice(1);
+
+    if (config.cookie_auth && !config.cookie_auth == false) {
+
+        if (req.headers['cookie'] && req.headers['cookie'].match(config.cookie_auth)) return Unblocker.app(req, res, next);
+    
+        res.send(fs.readFileSync('./error.html', { encoding: 'utf8' }).replace('%ERR%', 'Authorization required'));
+
+        res.statusCode = 400;
+
+    } else Unblocker.app(req, res, next);
+    
+});
+
+app.post(`/session/`, async(req, res, next) => {
+
+    req.body = await new Promise(resolve => {
+ 
+        var body = '';
+
+        req.on('data', chunk => body += chunk).on('end', () => {
+
+            try {
+
+                if (body.startsWith('{') && body.endsWith('}')) { resolve(JSON.parse(body)) }
+
+                else {
+
+            resolve(querystring.parse(body));
+
+          };
+
+        } catch(err) { resolve({}) }  
+
+
+        });
+
+      });
+
+    if (req.body.url) {
+
+        if (req.body.url.startsWith('//')) { req.body.url = 'http:' + req.body.url; } else if (req.body.url.startsWith('https://') || req.body.url.startsWith('http://')) { req.body.url = req.body.url } else { req.body.url = 'http://' + req.body.url};
+
+        if (config.cookie_auth && !config.cookie_auth == false) {
+
+            res.set('Set-Cookie', config.cookie_auth + `; path=${config.prefix};`);
+
+        };
+
+        return res.redirect(config.prefix + rewrite.url(req.body.url));
+
+    } else next();
+
+
+});
+
+// Frontend.
+
+app.use('/', express.static('public'));
+
+// WebSocket handler.
+
+Unblocker.ws(server);    
+
+server.listen(process.env.PORT || config.port, () => console.log(`Running on ${protocol}0.0.0.0:${config.port}`));
